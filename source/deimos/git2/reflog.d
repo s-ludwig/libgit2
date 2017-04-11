d0e7f39adbd9780865e3443acd17d128e4aafc89
Wiktor Żurawik <w.zurawik@nullthepointer.com>
2017-04-11 14:00:52 +0200

    Update module deimos.git2.reflog
    git_reflog_append_to function binding was removed.
    git_reflog_entry_byindex function binding was updated.

--- source/deimos/git2/reflog.d
+++ source/deimos/git2/reflog.d
@@ -9,11 +9,10 @@ extern (C):
 int git_reflog_read(git_reflog **out_, git_repository *repo,  const(char)* name);
 int git_reflog_write(git_reflog *reflog);
 int git_reflog_append(git_reflog *reflog, const(git_oid)* id, const(git_signature)* committer, const(char)* msg);
-int git_reflog_append_to(git_repository *repo, const(char)* name, const(git_oid)* id, const(git_signature)* committer, const(char)* msg);
 int git_reflog_rename(git_repository *repo, const(char)* old_name, const(char)* name);
 int git_reflog_delete(git_repository *repo, const(char)* name);
 size_t git_reflog_entrycount(git_reflog *reflog);
-const(git_reflog_entry)*  git_reflog_entry_byindex(git_reflog *reflog, size_t idx);
+const(git_reflog_entry)* git_reflog_entry_byindex(const(git_reflog)* reflog, size_t idx);
 int git_reflog_drop(
 	git_reflog *reflog,
 	size_t idx,
