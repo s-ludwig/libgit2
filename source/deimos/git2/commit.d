820294382967fc2c3ba1160f956a652720a7aa3b
Wiktor Żurawik <w.zurawik@nullthepointer.com>
2017-06-20 00:32:11 +0200

    Update module deimos.git2.commit
    Added bindings for:
        * git_commit_summary
        * git_commit_body
        * git_commit_header_field
        * git_commit_extract_signature
        * git_commit_amend
        * git_commit_create_buffer
        * git_commit_create_with_signature
        * git_commit_dup
    
    Updated bindings:
        * git_commit_create
        * git_commit_create_v

--- source/deimos/git2/commit.d
+++ source/deimos/git2/commit.d
@@ -15,6 +15,9 @@ git_repository * git_commit_owner(const(git_commit)* commit);
 const(char)* git_commit_message_encoding(const(git_commit)* commit);
 const(char)* git_commit_message(const(git_commit)* commit);
 const(char)* git_commit_message_raw(const(git_commit)* commit);
+const(char)* git_commit_summary(git_commit *commit);
+const(char)* git_commit_body(git_commit *commit);
+
 git_time_t git_commit_time(const(git_commit)* commit);
 int git_commit_time_offset(const(git_commit)* commit);
 const(git_signature)* git_commit_committer(const(git_commit)* commit);
@@ -34,6 +37,14 @@ int git_commit_nth_gen_ancestor(
 	git_commit **ancestor,
 	const(git_commit)* commit,
 	uint n);
+int git_commit_header_field(git_buf *out_, const(git_commit)* commit, const(char)* field);
+int git_commit_extract_signature(
+    git_buf *signature,
+    git_buf *signed_data,
+    git_repository *repo,
+    git_oid *commit_id,
+    const(char)* field,
+);
 int git_commit_create(
 	git_oid *id,
 	git_repository *repo,
@@ -43,7 +54,7 @@ int git_commit_create(
 	const(char)* message_encoding,
 	const(char)* message,
 	const(git_tree)* tree,
-	int parent_count,
+	size_t parent_count,
 	const(git_commit)** parents);
 int git_commit_create_v(
 	git_oid *id,
@@ -54,5 +65,34 @@ int git_commit_create_v(
 	const(char)* message_encoding,
 	const(char)* message,
 	const(git_tree)* tree,
-	int parent_count,
+	size_t parent_count,
 	...);
+int git_commit_amend(
+    git_oid *id,
+    const(git_commit)* commit_to_amend,
+    const(char)* update_ref,
+    const(git_signature)* author,
+    const(git_signature)* committer,
+    const(char)* message_encoding,
+    const(char)* message,
+    const(git_tree)* tree,
+);
+int git_commit_create_buffer(
+    git_buf *out_,
+    git_repository *repo,
+    const(git_signature)* author,
+    const(git_signature)* committer,
+    const(char)* message_encoding,
+    const(char)* message,
+    const(git_tree)* tree,
+    size_t parent_count,
+    const(git_commit)* parents[]
+);
+int git_commit_create_with_signature(
+    git_oid *out_,
+    git_repository *repo,
+    const(char)* commit_content,
+    const(char)* signature,
+    const(char)* signature_field,
+);
+int git_commit_dup(git_commit **out_, git_commit *source);
